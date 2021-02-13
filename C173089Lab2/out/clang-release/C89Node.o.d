2739388a0f761025286c0c0282bcdf1886d69036
Asif10H <habibullahasif71@gmail.com>
2021-02-14 00:35:09 +0600

    lab 2

---
+++ C173089Lab2/out/clang-release/C89Node.o.d
@@ -0,0 +1,4 @@
+out/clang-release//C89Node.o: C89Node.cc \
+  G:/googleeee/omnetpp-5.6.2-src-windows/omnetpp-5.6.2/include\omnetpp.h
+
+G:/googleeee/omnetpp-5.6.2-src-windows/omnetpp-5.6.2/include\omnetpp.h:
